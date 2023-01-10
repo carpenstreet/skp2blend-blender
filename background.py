279e90fedf750bb61d96d9a7cd50a533b4943082
habi <habijung0@gmail.com>
2023-01-10 17:23:36 +0900

    feat: 백그라운드로 실행할 때 Camera, Sun을 제외한 오브젝트 삭제

--- background.py
+++ background.py
@@ -7,6 +7,15 @@ argv = argv[argv.index("--") + 1:]  # get all args after "--"
 filename = argv[0]
 name, ext = os.path.splitext(filename)
 
+
+def init_load_setting():
+    # 기존 오브젝트(Camera, Sun 제외) 지우기
+    bpy.ops.object.select_by_type(extend=False, type="EMPTY")
+    bpy.ops.object.select_by_type(extend=True, type="MESH")
+    bpy.ops.object.delete()
+
+
+init_load_setting()
 bpy.ops.preferences.addon_enable(module="io_skp")
 bpy.ops.acon3d.import_skp(filepath=filename)
 bpy.ops.wm.save_mainfile(filepath=name + ".blend")
